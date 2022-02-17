f29846b22a7cf4a17c0a0ab775f22c18da814634
NikitaKakurin <neyasut87@rambler.ru>
2022-02-17 14:23:14 +0300

    feat: add scores, moving end ending

--- random-game/src/Script2048/g2048.js
+++ random-game/src/Script2048/g2048.js
@@ -6,20 +6,26 @@ export class Game2048{
         this.board = document.querySelector(selector),
         this.squares = document.querySelectorAll('.game_2048-item-square'),
         this.containerSquares = document.querySelector('.game_2048-container'),
+        this.containerActiveSquares = document.querySelector('.game_2048-container-active-square'),
+        this.scoreTable = document.querySelector('.game_2048-score__value');
         this.countInRow = Math.sqrt(this.squares.length),
         this.squaresCoords = []
+        this.isNextSquareShow=false;
+
     }
     
     initGame(){
         this.setCoords()
-
+        this.initTime = Date.now();
+        this.timer = 0;
+        this.score = 0;
         this.squareSizes = {
             'width':this.squares[0].clientWidth,
             'height':this.squares[0].clientHeight,
             };
 
 
-        
+        this.containerActiveSquares.innerHTML="";
         this.ArrayOfSquares = [
             [null, null, null, null],
             [null, null, null, null],
@@ -28,8 +34,6 @@ export class Game2048{
         ]
 
         this.createActiveSquare(this.choseRandomActiveSquare())
-
-
     }
 
     setCoords(){
@@ -67,12 +71,70 @@ export class Game2048{
         ASquare.style.height = this.squareSizes.height + 'px';
         ASquare.style.top = this.squaresCoords[squarePlace.row][squarePlace.column].top + 'px';
         ASquare.style.left = this.squaresCoords[squarePlace.row][squarePlace.column].left + 'px';
+        ASquare.dataset.value = 2;
+        ASquare.dataset.isCanChange = true;
         this.ArrayOfSquares[squarePlace.row][squarePlace.column]=ASquare;
         ASquare.hidden=true;
-        this.containerSquares.append(ASquare);
-        setTimeout(()=>ASquare.hidden=false, 300);
+        this.containerActiveSquares.append(ASquare);
+
+        setTimeout(()=>{
+            ASquare.hidden=false;
+            this.isNextSquareShow=true;
+            this.setScore();
+            if(this.arrayOfEmpty.length==1 && this.isGameOver()){
+                setTimeout(()=>handleGameOver(),200)
+            }
+        }, 300);
     }
 
+    handleGameOver(){
+
+    }
+
+    isGameOver(){
+        for(let row = 0; row<this.countInRow; row++){
+            for(let column = 0; column < this.countInRow; column++){
+                let currentSquareValue=this.ArrayOfSquares[row][column].dataset.value;
+                if(row !== 0){
+                    if(currentSquareValue==this.ArrayOfSquares[row-1][column].dataset.value){
+                        return false;
+                    }
+                }
+                
+                if(row !== this.countInRow-1){
+                    if(currentSquareValue==this.ArrayOfSquares[row+1][column].dataset.value){
+                        return false;
+                    };
+                }
+
+                if(column !== 0){
+                    if(currentSquareValue==this.ArrayOfSquares[row][column-1].dataset.value){
+                        return false;
+                    }
+                }
+                
+                if(column !== this.countInRow-1){
+                    if(currentSquareValue==this.ArrayOfSquares[row][column+1].dataset.value){
+                        return false;
+                    };
+                }
+            }
+        }
+        return true;
+
+    }
+    checkMergeNeighbors(row,column){
+        this.ArrayOfSquares[row][column]==this.ArrayOfSquares[row-1][column];
+        this.ArrayOfSquares[row][column]==this.ArrayOfSquares[row+1][column];
+        
+        this.ArrayOfSquares[row][column]==this.ArrayOfSquares[row-1][column];
+        this.ArrayOfSquares[row][column]==this.ArrayOfSquares[row+1][column];
+    }
+
+    setScore(){
+        this.score += 2;
+        this.scoreTable.innerText=this.score;
+    }
     getIndexesEmptySquares(){
         this.arrayOfEmpty = [];
         this.ArrayOfSquares.forEach((row, index)=>{
@@ -86,103 +148,162 @@ export class Game2048{
     };
     
     moveUp(){
-        for(let column = 0; column < this.countInRow; column++){
-            let stepsUp = 0;
-            for(let row = 0; row<this.countInRow; row++){
-                if(this.ArrayOfSquares[row][column]===null){
-                    stepsUp++
-                }else if(this.ArrayOfSquares[row][column]!==null){
-                    if(stepsUp===0){
-                        if((row - (stepsUp+1)>=0) && this.ArrayOfSquares[row - (stepsUp+1)][column]!==null){
-                            if(this.ArrayOfSquares[row - (stepsUp+1)][column].innerText == this.ArrayOfSquares[row][column].innerText){
-                                this.changeArrayOfSquares(row, column, row - (stepsUp+1), column , true);
-                                stepsUp++;
-                            }
-                        }else{
-                            continue;
-                        }
-                    }else if((row - (stepsUp+1)>=0) && this.ArrayOfSquares[row - (stepsUp+1)][column]!==null){
-                        if(this.ArrayOfSquares[row - (stepsUp+1)][column].innerText == this.ArrayOfSquares[row][column].innerText){
-                            this.changeArrayOfSquares(row, column, row - (stepsUp+1), column , true);
-                            stepsUp++;
-                        }else{
-                            this.changeArrayOfSquares(row, column, row - stepsUp, column, false)
-                        }
-                    }else{
-                        this.changeArrayOfSquares(row, column, row - stepsUp, column, false)
-                    }
-
-
-                }
-            }
+        if(this.isNextSquareShow==false){
+            return;
         }
-        this.createActiveSquare(this.choseRandomActiveSquare())
-    }
-
-    moveDown(){
         let isChange = false;
         for(let column = 0; column < this.countInRow; column++){
-            let stepsUp = 0;
-            for(let row = this.countInRow-1; row>=0; row--){
-                if(this.ArrayOfSquares[row][column]===null){
-                    stepsUp++
-                }else if(this.ArrayOfSquares[row][column]!==null){
-                    if(stepsUp===0){
-                        if((row + (stepsUp+1)<=3) && this.ArrayOfSquares[row + (stepsUp+1)][column]!==null){
-                            if(this.ArrayOfSquares[row + (stepsUp+1)][column].innerText == this.ArrayOfSquares[row][column].innerText){
-                                this.changeArrayOfSquares(row, column, row + (stepsUp+1), column , true);
-                                stepsUp++;
-                                isChange=true;
-                            }
-                        }else{
-                            continue;
-                        }
-                    }else if((row + (stepsUp+1)<=3) && this.ArrayOfSquares[row + (stepsUp+1)][column]!==null){
-                        if(this.ArrayOfSquares[row + (stepsUp+1)][column].innerText == this.ArrayOfSquares[row][column].innerText){
-                            this.changeArrayOfSquares(row, column, row + (stepsUp+1), column , true);
-                            stepsUp++;
+            let steps = 0;
+            for(let row = 0; row<this.countInRow; row++){
+                if(this.ArrayOfSquares[row][column]!==null){
+                    if((row - (steps+1)>=0)
+                        && this.ArrayOfSquares[row - (steps+1)][column]!==null
+                        && this.ArrayOfSquares[row - (steps+1)][column].dataset.isCanChange
+                        && this.ArrayOfSquares[row - (steps+1)][column].dataset.value == this.ArrayOfSquares[row][column].dataset.value){
+                            this.changeArrayOfSquares(row, column, row - (steps+1), column , true);
+                            steps++;
                             isChange=true;
-                        }else{
-                            this.changeArrayOfSquares(row, column, row + stepsUp, column, false)
-                            isChange=true;
-                        }
-                    }else{
-                        this.changeArrayOfSquares(row, column, row + stepsUp, column, false)
+                    }else if(steps>0){
+                        this.changeArrayOfSquares(row, column, row - steps, column, false)
                         isChange=true;
                     }
+                }else{
+                    steps++
                 }
             }
         }
         if(isChange){
-            this.createActiveSquare(this.choseRandomActiveSquare())
+            this.isNextSquareShow=false;
+            this.createActiveSquare(this.choseRandomActiveSquare());
         }
-        
     }
 
+    moveDown(){
+        if(this.isNextSquareShow==false){
+            return;
+        }
+        let isChange = false;
+        for(let column = 0; column < this.countInRow; column++){
+            let steps = 0;
+            for(let row = this.countInRow-1; row>=0; row--){
+                if(this.ArrayOfSquares[row][column]!==null){
+                    if((row + (steps+1)<this.countInRow)
+                        && this.ArrayOfSquares[row + (steps+1)][column]!==null 
+                        && this.ArrayOfSquares[row + (steps+1)][column].dataset.isCanChange
+                        && this.ArrayOfSquares[row + (steps+1)][column].dataset.value == this.ArrayOfSquares[row][column].dataset.value){
+                            this.changeArrayOfSquares(row, column, row + (steps+1), column , true);
+                            steps++;
+                            isChange=true;
+                    }else if(steps>0){
+                        this.changeArrayOfSquares(row, column, row + steps, column, false)
+                        isChange=true;
+                    }
+                }else{
+                    steps++
+                }
+            }
+        }
+        if(isChange){
+            this.isNextSquareShow=false;
+            this.createActiveSquare(this.choseRandomActiveSquare());
+        }
+    }
+
+    moveLeft(){
+        if(this.isNextSquareShow==false){
+            return;
+        }
+        let isChange = false;
+        for(let row = 0; row < this.countInRow; row++){
+            let steps = 0;
+            for(let column = 0; column<this.countInRow; column++){
+                if(this.ArrayOfSquares[row][column]!==null){
+                    if((column - (steps+1)>=0) 
+                        && this.ArrayOfSquares[row][column - (steps+1)]!==null
+                        && this.ArrayOfSquares[row][column - (steps+1)].dataset.isCanChange 
+                        && this.ArrayOfSquares[row][column - (steps+1)].dataset.value == this.ArrayOfSquares[row][column].dataset.value){
+                            this.changeArrayOfSquares(row, column, row, column - (steps+1) , true);
+                            steps++;
+                            isChange=true;
+                    }else if(steps>0){
+                        this.changeArrayOfSquares(row, column, row, column - steps, false)
+                        isChange=true;
+                    }
+                }else{
+                    steps++
+                }
+            }
+        }
+        if(isChange){
+            this.isNextSquareShow=false;
+            this.createActiveSquare(this.choseRandomActiveSquare());
+        }
+    }
+
+    moveRight(){
+        if(this.isNextSquareShow==false){
+            return;
+        }
+        let isChange = false;
+        for(let row = 0; row < this.countInRow; row++){
+            let steps = 0;
+            for(let column= this.countInRow-1; column>=0; column--){
+                if(this.ArrayOfSquares[row][column]!==null){
+                    if((column + (steps+1)<this.countInRow) 
+                        && this.ArrayOfSquares[row][column + (steps+1)]!==null
+                        && this.ArrayOfSquares[row][column + (steps+1)].dataset.isCanChange
+                        && this.ArrayOfSquares[row][column + (steps+1)].dataset.value == this.ArrayOfSquares[row][column].dataset.value){
+                            this.changeArrayOfSquares(row, column, row, column + (steps+1) , true);
+                            steps++;
+                            isChange=true;
+                    }else if(steps>0){
+                        this.changeArrayOfSquares(row, column, row, column + steps, false)
+                        isChange=true;
+                    }
+                }else{
+                    steps++
+                }
+            }
+        }
+        if(isChange){
+            this.isNextSquareShow=false;
+            this.createActiveSquare(this.choseRandomActiveSquare());
+        }
+    }
 
 
     changeArrayOfSquares(currentRow, currentColumn, targetRow, targetColumn, isMergeSquare){
         let previousSquare = this.ArrayOfSquares[targetRow][targetColumn];
         this.ArrayOfSquares[targetRow][targetColumn]=this.ArrayOfSquares[currentRow][currentColumn];
         this.ArrayOfSquares[currentRow][currentColumn]=null
-        this.moveSquareUp(targetRow, targetColumn, previousSquare, isMergeSquare)
+        this.moveSquare(targetRow, targetColumn, previousSquare, isMergeSquare)
     }
 
-    moveSquareUp(targetRow, targetColumn, previousSquare, isMergeSquare){
+    moveSquare(targetRow, targetColumn, previousSquare, isMergeSquare){
         let prevSquare = previousSquare;
         const currentSquare = this.ArrayOfSquares[targetRow][targetColumn]
         const handleTransitionEnd = (event)=>{
+            currentSquare.removeEventListener('transitionend', handleTransitionEnd)
             if(prevSquare){
                 prevSquare.remove();
                 prevSquare = null; 
             }
             currentSquare.innerText = (+currentSquare.innerText)*2;
-            currentSquare.removeEventListener('transitionend', handleTransitionEnd)
+            this.changeColor(currentSquare);
+            currentSquare.dataset.isCanChange = true;
         }
         if(isMergeSquare){
+            currentSquare.dataset.value = (+currentSquare.dataset.value) * 2;
+            currentSquare.dataset.isCanChange = false;
             currentSquare.addEventListener('transitionend', handleTransitionEnd)
         }
         currentSquare.style.top = this.squaresCoords[targetRow][targetColumn].top + 'px';
+        currentSquare.style.left = this.squaresCoords[targetRow][targetColumn].left + 'px';
+    }
+
+    changeColor(square){
+        square.classList.remove(`game_2048-active-square-${+square.innerText/2}`);
+        square.classList.add(`game_2048-active-square-${square.innerText}`)
     }
 
 
--- random-game/src/index.js
+++ random-game/src/index.js
@@ -4,32 +4,97 @@ import './style/normalize.css'
 import { Game2048 } from './Script2048/g2048';
 
 let log = (param)=> console.log(param);
+
 const btnNewGame = document.querySelector('game_new-game-btn');
 const g2048 = new Game2048('.game_2048-container');
-document.addEventListener('click', handleClick)
 
-document.addEventListener('keydown',  handleKeyDown)
+document.addEventListener('click', handleClick);
+document.addEventListener('mousedown', handleMouseDown);
+document.addEventListener('keydown', handleKeyDown);
 
 function handleKeyDown(event){
-    log(event.code);
-    if(event.code==="KeyW"){
+    if(event.code==="KeyW" || event.code==="ArrowUp"){
         g2048.moveUp()
     }
+    if(event.code==="KeyS" || event.code==="ArrowDown"){
+        g2048.moveDown()
+    }
+    if(event.code==="KeyA" || event.code==="ArrowLeft"){
+        g2048.moveLeft()
+    }
+    if(event.code==="KeyD" || event.code==="ArrowRight"){
+        g2048.moveRight()
+    }
 }
+
+function handleMouseDown(event){
+    const target =  event.target
+
+    if(target.closest('.game_2048-container')){
+        getStartSwipe(event);
+    }
+}
+
 
 function handleClick(event){
     const target =  event.target
     if(target.classList.contains('game_new-game-btn')){
         g2048.initGame();
     }
+
     if(target.classList.contains('up')){
         g2048.moveUp();
-        return;
+         return;
     }
     if(target.classList.contains('down')){
-        g2048.moveDown()
+        g2048.moveDown();
         return;
     }
-
+    if(target.classList.contains('left')){
+        g2048.moveLeft();
+        return;
+    }
+    if(target.classList.contains('right')){
+        g2048.moveRight();
+        return;
+    }
 }
+
+function getStartSwipe(event){
+  const initX = event.clientX;
+  const initY = event.clientY;
+
+  document.addEventListener("mouseup",handleEndSwipe);
+
+  function handleEndSwipe(event){
+    const endX = event.clientX;
+    const endY = event.clientY;
+    const shiftX = endX - initX;
+    const shiftY = endY - initY;
+
+    if(Math.abs(shiftX)>Math.abs(shiftY)){
+        getHorizontalDirection()
+    }else if(Math.abs(shiftX)<Math.abs(shiftY)){
+        getVerticalDirection()
+    }
+
+    function getHorizontalDirection(){
+        if(shiftX>30){
+            g2048.moveRight();
+        }else if(shiftX<-30){
+            g2048.moveLeft();
+        }
+    }
+
+    function getVerticalDirection(){
+        if(shiftY>30){
+            g2048.moveDown();
+        }else if(shiftY<-30){
+            g2048.moveUp();
+        }
+    }
+    document.removeEventListener("mouseup",handleEndSwipe);
+  }
+}
+
 
